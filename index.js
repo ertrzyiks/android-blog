aa109cad0b412f144686c8676d1b93dd087c5ffc
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-01-09 00:13:27 +0100

    Handle social links in bio

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var ghost = require('ghost'),
-    path = require('path');
+    path = require('path'),
+    hbs = require('ghost/node_modules/express-hbs'),
+
+    htmlencode = require('htmlencode'),
+    widget = new htmlencode.Encoder('numerical');
 
 ghost({
     config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
+    hbs.registerHelper('showBio', function showBio(bio) {
+        var safeBio = widget.htmlEncode(bio),
+            newBio = safeBio
+                .replace(/https?:\/\/github.com\/([a-z0-9]+)/gi, function (match, username) {
+                    return '<a class="social-link" href="' + match + '"><i class="fa fa-github"></i> Github</a>';
+                })
+                .replace(/https?:\/\/stackoverflow.com\/users\/([0-9]+)\/([a-z0-9]+)/gi, function (match, id, username) {
+                    return '<a class="social-link" href="' + match + '"><i class="fa fa-stack-overflow"></i> Stack Overflow</a>';
+                })
+            ;
+
+        return new hbs.SafeString(newBio);
+    });
+
     ghostServer.start();
 });
